9c1e416c5af8f4a4ec23c2b27ff976b6c6b15142
siumhossain <sium.hossain@yahoo.com>
2022-03-11 02:17:31 +0600

    done

--- find_your_age.py
+++ find_your_age.py
@@ -25,22 +25,55 @@ currentDay :int= datetime.now().day
 currentMonth :int= datetime.now().month
 currentYear :int= datetime.now().year
 
+def checkAge(message):
+    
+       
+    
+    while True:
+        try:
+            birth_day = int(input(message))
 
-while True:
-    try:
-        
-        birth_day = int(input("birth_day? ex:1-31 \n"))
-        if birth_day not in range(1,32):
-            print('wrong date [date should be in 1-31]')
-            birth_day = int(input("birth_day? ex:1-31 \n"))
-        
-        birth_month = int(input("birth_month? ex:1-12 \n"))
-        if birth_month not in range(1,13):
-            print('wrong month [date should be in 1-12]')
-            birth_month = int(input("birth_month? ex:1-12 \n"))
-        birth_year = int(input("birth_year? ex:1950\n"))
-        findAge(currentDay,currentMonth,currentYear,birth_day,birth_month,birth_year)
-        break
-    except ValueError:
-        print("the input must be in number")
+        except ValueError:
+            print("the input must be in number")
+        else:
+            if birth_day not in range(1,32):
+                print('wrong date [date should be in 1-31]')
+            else:
+                return birth_day
+def checkMonth(message):
+    
+       
+    
+    while True:
+        try:
+            birth_month = int(input(message))
+
+        except ValueError:
+            print("the input must be in number")
+        else:
+            if birth_month not in range(1,13):
+                print('wrong date [Month should be in 1-12]')
+            else:
+                return birth_month
+def checkYear(message):
+    
+       
+    
+    while True:
+        try:
+            birth_year = int(input(message))
+
+        except ValueError:
+            print("the input must be in number")
+        else:
+            if len(str(birth_year)) != 4:
+                print('wrong year [year should be in 4 digits]')
+            else:
+                return birth_year
+birth_day = checkAge("Day?\n")
+birth_month = checkMonth("Month?\n")
+birth_year =  checkYear("Year?\n")
+
+
+findAge(currentDay,currentMonth,currentYear,birth_day,birth_month,birth_year)
 
